f51964f45076bd17f396af7017152f732cf01ff9
Veixer <veix92@hotmail.com>
2023-04-20 14:00:39 +0300

    Added pages

---
+++ Pages/Timestamps.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AikaHalli.Pages
+{
+    public class TimestampsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
